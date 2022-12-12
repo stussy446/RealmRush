6a5309eababe6c08b290cd22c18ca71e782f63b4
Steve Rector <steverector27@gmail.com>
2022-12-12 15:03:52 -0800

    Adds ability for enemy to follow given list of waypoints

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> _path = new List<Waypoint>();
+    [SerializeField] private float _secondsDelay = 1f;
 
     private void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    private void PrintWaypointName()
+    private IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in _path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position; ;
+            yield return new WaitForSeconds(_secondsDelay);
         }
     }
 }
